9309f31fc163ec78c489c710980129d9c4a7f366
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-12-13 16:18:43 -0800

    no need to rebuild julia

---
+++ all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+docker build --pull -t skippa/coq coq
+docker build --pull -t skippa/coqide coqide
+
+docker build --pull -t skippa/anaconda anaconda
+docker build --pull -t skippa/julia julia
+docker build --pull -t skippa/ijulia ijulia
