6133852b45305d65368ab1de027be03cb5ff1470
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-12-25 19:58:38 -0800

    common /workspace, more packages for ijulia

---
+++ ijulia/install.jl
@@ -0,0 +1,33 @@
+Pkg.init()
+Pkg.update()
+
+for pkg in ["IJulia", 
+            # JuliaPro
+            "DataStructures",
+            "Calculus",
+            "DataFrames",
+            "StatsBase",
+            "Distributions",
+            "HypothesisTests",
+            "GLM",
+            "JuMP",
+            "Optim",
+            "Roots",
+#            "ODBC",
+#            "JDBC",
+            "Gadfly",
+            "PyPlot",
+            "Interact",
+            "PyCall",
+            "JSON",
+            "HDF5",
+            "JLD",
+            # Other Packages
+            "Iterators",
+            "BenchmarkTools"
+            ]
+  println("Installing ", pkg)
+  Pkg.add(pkg)
+  pkgsym = Symbol(pkg)
+  eval(:(using $pkgsym))
+end
